fc4f286b7f071882dc23a28ab2f787cc23310eaa
aliter230880 <56067954+aliter230880@users.noreply.github.com>
2026-05-09 01:02:00 +0300

    Update next.config.ts

--- web3gram-nextjs/next.config.ts
+++ web3gram-nextjs/next.config.ts
@@ -1,5 +1,10 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  trailingSlash: true,
+};
 
-const nextConfig: NextConfig = {};
-
-export default nextConfig;
+module.exports = nextConfig;
