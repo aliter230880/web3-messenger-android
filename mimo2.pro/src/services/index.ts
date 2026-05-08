562c430d1dbbf18c029e18e467d308da87bda527
aliter230880 <56067954+aliter230880@users.noreply.github.com>
2026-05-08 23:06:38 +0300

    Add files via upload

--- mimo2.pro/src/services/index.ts
+++ mimo2.pro/src/services/index.ts
@@ -1,9 +1,7 @@
 // Services
-export { walletService } from './walletService';
 export { xmtpService } from './xmtpService';
 export { identityService } from './identityService';
 export { transferService } from './transferService';
 
-export type { WalletConnection } from './walletService';
 export type { XmtpMessage } from './xmtpService';
 export type { Identity } from './identityService';
